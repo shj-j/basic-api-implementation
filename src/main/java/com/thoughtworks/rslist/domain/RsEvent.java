3e56b41925f0134c13f2926fb9a603f6d6448525
shj-j <shang.jie@outlook.com>
2020-08-04 21:42:39 +0800

    implement getRS, addRs

---
+++ src/main/java/com/thoughtworks/rslist/domain/RsEvent.java
@@ -0,0 +1,2 @@
+package com.thoughtworks.rslist.domain;public class RsEvent {
+}
---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,64 @@
+package com.thoughtworks.rslist.api;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.thoughtworks.rslist.domain.RsEvent;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+
+import java.awt.*;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class RsControllerTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    void getRsList() throws Exception{
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName", is("firstEvent")))
+                .andExpect(jsonPath("$[0].category", is("unCategory")))
+                .andExpect(jsonPath("$[1].eventName", is("secondEvent")))
+                .andExpect(jsonPath("$[1].category", is("unCategory")))
+                .andExpect(jsonPath("$[2].eventName", is("thirdEvent")))
+                .andExpect(jsonPath("$[2].category", is("unCategory")))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void addOneRs() throws Exception{
+
+        RsEvent rsEvent = new RsEvent("fourthEvent", "unCategory");
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String requestJson = objectMapper.writeValueAsString(rsEvent);
+
+        mockMvc.perform(post("/rs/event")
+                .content(requestJson).contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/rs/list"))
+                .andExpect(jsonPath("$[0].eventName", is("firstEvent")))
+                .andExpect(jsonPath("$[0].category", is("unCategory")))
+                .andExpect(jsonPath("$[1].eventName", is("secondEvent")))
+                .andExpect(jsonPath("$[1].category", is("unCategory")))
+                .andExpect(jsonPath("$[2].eventName", is("thirdEvent")))
+                .andExpect(jsonPath("$[2].category", is("unCategory")))
+                .andExpect(jsonPath("$[3].eventName", is("fourthEvent")))
+                .andExpect(jsonPath("$[3].category", is("unCategory")))
+                .andExpect(status().isOk());
+
+    }
+
+
+}
